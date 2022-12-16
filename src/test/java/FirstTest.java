3ad0731c4cd801b308c5c4ee9996c9b7d868b586
Denis Pershin <denis.pershin@nordigy.ru>
2022-12-16 16:56:00 +0400

    extract common class

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,5 @@
+import java.util.logging.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,38 +10,32 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class FirstTest extends Common {
 
+    private final Logger logger = Logger.getLogger(FirstTest.class.getName());
+
     @Test
     public void getErrorPage() throws Exception {
-        // Создаем экземпляр WebDriver
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://checker.jettycloud.com/");
+        webDriver.get(baseUrl);
         // Находим элемент по названию класса
 
-        String title = driver.getTitle();
-        System.out.println("Page title is: " + title);
+        String title = webDriver.getTitle();
+        logger.info("Page title is: " + title);
         assertEquals(title, "DINS QA Boot Camp check tool");
-        String errorCode = driver.findElement(By.className("ant-result-title")).getText();
+        String errorCode = webDriver.findElement(By.className("ant-result-title")).getText();
         assertEquals(errorCode, "404");
-        String errorText = driver.findElement(By.className("ant-result-subtitle")).getText();
+        String errorText = webDriver.findElement(By.className("ant-result-subtitle")).getText();
         assertEquals(errorText, "Стринца не найдена");
-        // Закрываем браузер
-        driver.close();
     }
 
     @Test
     public void getErrorPageWithPageObject() throws Exception {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://checker.jettycloud.com/");
-
-        String title = driver.getTitle();
-        System.out.println("Page title is: " + title);
+        webDriver.get(baseUrl);
+        String title = webDriver.getTitle();
+        logger.info("Page title is: " + title);
         assertEquals(title, "DINS QA Boot Camp check tool");
-        ErrorPage errorPage = new ErrorPage(driver);
+        ErrorPage errorPage = new ErrorPage(webDriver);
         String errorCode = errorPage.getErrorCode();
         assertEquals(errorCode, "404");
         String errorText = errorPage.getErrorText();
         assertEquals(errorText, "Стринца не найдена");
-        // Закрываем браузер
-        driver.close();
     }
 }
